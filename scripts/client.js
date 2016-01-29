61446e04b07f051629057a58545fd4dc7869d67c
James Baicoianu <james_github@baicoianu.com>
2016-01-28 22:04:25 -0800

    Added admin URL option, brighten lights, added about page stub

--- scripts/client.js
+++ scripts/client.js
@@ -1,7 +1,18 @@
 elation.require(['engine.engine', 'engine.things.player', 'engine.things.light', 'janusweb.janusweb'], function() {
   elation.component.add('janusweb.client', function() {
     this.initEngine = function() {
-      //this.enginecfg.systems.push('admin');
+      var hashargs = elation.url();
+       
+      this.enginecfg.systems = [];
+      this.enginecfg.systems.push("physics");
+      this.enginecfg.systems.push("world");
+      this.enginecfg.systems.push("ai");
+      if (hashargs.admin == 1) {
+        this.enginecfg.systems.push("admin");
+      } 
+      this.enginecfg.systems.push("render");
+      this.enginecfg.systems.push("sound");
+      this.enginecfg.systems.push("controls");
     }
     this.initWorld = function() {
       var things = this.world.load({
@@ -22,7 +33,7 @@ elation.require(['engine.engine', 'engine.things.player', 'engine.things.light',
             type: 'light_directional',
             properties: {
               position: [-20,50,25],
-              intensity: 0.1
+              intensity: 0.2
             }
           },
           point: {
@@ -30,7 +41,7 @@ elation.require(['engine.engine', 'engine.things.player', 'engine.things.light',
             type: 'light_point',
             properties: {
               position: [22,19,-15],
-              intensity: 0.1
+              intensity: 0.2
             }
           },
           player: {
@@ -44,6 +55,11 @@ elation.require(['engine.engine', 'engine.things.player', 'engine.things.light',
         }
       });
       this.player = things.children.janusweb.children.player;
+    }
+    this.showAbout = function() {
+      var aboutwin = elation.ui.window({append: document.body, center: true, title: 'About JanusWeb'});
+      var frame = elation.ui.iframe({src: 'http://github.com/jbaicoianu/janusweb/', classname: 'janusweb_about'});
+      aboutwin.setcontent(frame);
     }
   }, elation.engine.client);
 });
