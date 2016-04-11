e7f6513d60ab1df98ae61845baa3328ce8677f60
James Baicoianu <james_github@baicoianu.com>
2016-04-11 02:28:08 -0700

    Updated config settings for release

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,8 @@
-elation.config.set('dependencies.path', 'http://meobets.com/~bai/januswebtest/');
-elation.config.set('dependencies.main', 'bundle.js');
-elation.config.set('engine.assets.font.path', 'http://meobets.com/~bai/januswebtest/media/fonts/');
-elation.config.set('janusweb.network.host', 'ws://ec2-54-148-60-8.us-west-2.compute.amazonaws.com:9001');
+elation.config.set('dependencies.protocol', 'http:'); //document.location.protocol);
+elation.config.set('dependencies.host', 'janusweb.metacade.com');
+elation.config.set('dependencies.path', elation.config.get('dependencies.protocol') + '//' + elation.config.get('dependencies.host') + '/');
+elation.config.set('dependencies.main', 'janusweb.js');
+elation.config.set('engine.assets.font.path', elation.config.get('dependencies.path') + 'media/fonts/');
+elation.config.set('janusweb.network.host', 'wss://janusweb.lnq.to:5567');
+elation.config.set('janusweb.network.corsproxy', 'http://janusweb.lnq.to:8089/');
+elation.config.set('janusweb.datapath', elation.config.get('dependencies.path') + 'media/');
