e34881ab340cf0012f4e44f4a7dbbc48bf409f7a
James Baicoianu <james_github@baicoianu.com>
2016-04-15 04:40:01 -0700

    Updated config

--- scripts/config.js
+++ scripts/config.js
@@ -2,7 +2,9 @@ elation.config.set('dependencies.protocol', 'http:'); //document.location.protoc
 elation.config.set('dependencies.host', 'janusweb.metacade.com');
 elation.config.set('dependencies.path', elation.config.get('dependencies.protocol') + '//' + elation.config.get('dependencies.host') + '/');
 elation.config.set('dependencies.main', 'janusweb.js');
-elation.config.set('engine.assets.font.path', elation.config.get('dependencies.path') + 'media/fonts/');
-elation.config.set('janusweb.network.host', 'wss://janusweb.lnq.to:5567');
+elation.config.set('janusweb.network.host', 'ws://janusweb.lnq.to:5566');
 elation.config.set('janusweb.network.corsproxy', 'http://janusweb.lnq.to:8089/');
 elation.config.set('janusweb.datapath', elation.config.get('dependencies.path') + 'media/');
+elation.config.set('janusweb.tracking.enabled', true);
+elation.config.set('janusweb.tracking.clientid', 'UA-49582649-2');
+elation.config.set('engine.assets.font.path', elation.config.get('janusweb.datapath') + 'fonts/');
