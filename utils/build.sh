a1603ea3be6a944142d9a6aa75fdccf5937deed4
James Baicoianu <james_github@baicoianu.com>
2016-05-16 02:21:16 -0700

    Added build parameter to specify config file

--- utils/build.sh
+++ utils/build.sh
@@ -8,10 +8,15 @@ if [ -e build/media ]; then
   rm -r build/media
 fi
 
+CFGNAME="$1"
+if [ -z "$CFGNAME" ]; then
+  CFGNAME="config"
+fi
+
 cp -r `pwd`/media build/media
 mv build/media/index.html build/index.html
 cd ../../
-./elation component runjs utils.pack -config janusweb.config -bundle janusweb ui.themes.dark janusweb.client engine.assetworker
+./elation component runjs utils.pack -config janusweb.$CFGNAME -bundle janusweb janusweb.client engine.assetworker
 mv janusweb.css janusweb.js components/janusweb/build
 echo 'Built new release in ./build/'
 
