a412fc1e9edd355e71d0b55ca6c1ad60f6d8dcf8
James Baicoianu <james_github@baicoianu.com>
2016-05-29 12:24:56 -0700

    Updated build script to support prroject-local Elation checkout

--- utils/build.sh
+++ utils/build.sh
@@ -15,7 +15,13 @@ fi
 
 cp -r `pwd`/media build/media
 mv build/media/index.html build/index.html
-cd ../../
+if [ -e elation ] && [ -e elation/components/janusweb ]; then
+  echo 'Building from project-local elation directory'
+  cd elation/
+else
+  echo 'Building from parent directory'
+  cd ../../
+fi
 ./elation component runjs utils.pack -config janusweb.$CFGNAME -bundle janusweb janusweb.client engine.assetworker
 mv janusweb.css janusweb.js components/janusweb/build
 echo 'Built new release in ./build/'
