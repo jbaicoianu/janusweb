c267309711163dd3fe5faadd3155369c5c7f9a8f
James Baicoianu <james_github@baicoianu.com>
2017-01-02 22:18:48 -0800

    Added collider and websurface support

--- scripts/object.js
+++ scripts/object.js
@@ -1,4 +1,4 @@
-elation.require(['janusweb.janusbase'], function() {
+elation.require(['janusweb.janusbase', 'janusweb.websurface'], function() {
   elation.component.add('engine.things.janusobject', function() {
     this.sidemap = {
       'back': THREE.FrontSide,
@@ -11,6 +11,7 @@ elation.require(['janusweb.janusbase'], function() {
         janusid: { type: 'string', refreshGeometry: true },
         image_id: { type: 'string', set: this.updateMaterial },
         video_id: { type: 'string', set: this.updateMaterial },
+        url: { type: 'string' },
         loop: { type: 'boolean' },
         collision_id: { type: 'string' },
         websurface_id: { type: 'string', set: this.updateMaterial },
@@ -18,19 +19,40 @@ elation.require(['janusweb.janusbase'], function() {
         cull_face: { type: 'string', default: 'back', set: this.updateMaterial },
         blend_src: { type: 'string', default: 'src_alpha', set: this.updateMaterial },
         blend_dest: { type: 'string', default: 'one_minus_src_alpha', set: this.updateMaterial },
+        collider_id: { type: 'string', set: this.updatePhysics },
+        collider_pos: { type: 'vector3', set: this.updatePhysics },
+        collider_scale: { type: 'vector3', set: this.updatePhysics },
+        collision_static: { type: 'boolean', default: true, set: this.updatePhysics },
       });
       //elation.events.add(this, 'thing_init3d', elation.bind(this, this.assignTextures));
+
+      if (this.websurface_id) {
+        var websurface = this.room.websurfaces[this.websurface_id];
+        console.log('do a websurface: ' + this.websurface_id, websurface, this.room.baseurl);
+        if (websurface) {
+          var url = websurface.src;
+          if (url && !url.match(/^(https?:)?\/\//)) {
+            url = this.room.baseurl + url;
+          }
+          this.url = url;
+        }
+        this.pickable = false;
+        this.collidable = false;
+      }
     }
     this.createObject3D = function() {
       if (this.properties.exists === false) return;
 
       var object = null, geometry = null, material = null;
       if (this.janusid) {
-        object = elation.engine.assets.find('model', this.janusid);
-        if (object.userData.loaded) {
-          this.assignTextures();
-        } else {
-          elation.events.add(object, 'asset_load', elation.bind(this, this.assignTextures));
+        var asset = this.getAsset('model', this.janusid);
+        if (asset) {
+          if (asset.loaded) {
+            setTimeout(elation.bind(this, this.assignTextures), 0);
+          } else {
+            elation.events.add(asset, 'asset_load_complete', elation.bind(this, this.assignTextures));
+          }
+          object = asset.getInstance();
         }
       } else {
         object = new THREE.Object3D();
@@ -44,65 +66,75 @@ elation.require(['janusweb.janusbase'], function() {
     }
     this.createForces = function() {
       elation.engine.things.janusobject.extendclass.createForces.call(this);
+      if (!this.collidable) return;
 
-      if (this.collision_id) {
+      var collider_id = this.collider_id || this.collision_id;
+      var collider_scale = this.properties.collider_scale || this.properties.scale;
+      if (collider_id) {
+        if ((!this.collision_static || this.collision_static == 'false') && this.room.gravity) { // FIXME - should never receive 'false' as a string here
+          this.objects.dynamics.mass = this.mass = 1;
+          this.objects.dynamics.addForce('static', new THREE.Vector3(0, this.room.gravity, 0));
+        }
+
         this.collidable = true;
-        if (this.collision_id == 'sphere') {
-          this.setCollider('sphere', {radius: Math.max(this.scale.x, this.scale.y, this.scale.z)});
-        } else if (this.collision_id == 'cube') {
-          var halfsize = this.properties.scale.clone().multiplyScalar(.5);
+        if (collider_id == 'sphere') {
+          this.setCollider('sphere', {radius: Math.max(collider_scale.x, collider_scale.y, collider_scale.z) / 2});
+        } else if (collider_id == 'cube') {
+          var halfsize = collider_scale.clone().multiplyScalar(.5);
           this.setCollider('box', {min: halfsize.clone().negate(), max: halfsize});
-        } else if (this.collision_id == 'plane') {
-          var halfsize = this.properties.scale.clone().multiplyScalar(.5);
+        } else if (collider_id == 'plane') {
+          var halfsize = collider_scale.clone().multiplyScalar(.5);
+          halfsize.z = .1;
           this.setCollider('box', {min: halfsize.clone().negate(), max: halfsize});
-        } else if (this.collision_id == 'cylinder') {
-          this.setCollider('cylinder', {height: this.scale.y, radius: Math.max(this.scale.x, this.scale.z) / 2, offset: new THREE.Vector3(0, 0.5 * this.scale.y, 0)});
+        } else if (collider_id == 'cylinder') {
+          this.setCollider('cylinder', {height: collider_scale.y, radius: Math.max(collider_scale.x, collider_scale.z) / 2, offset: new THREE.Vector3(0, 0.5 * collider_scale.y, 0)});
         } else {
-          var collider = elation.engine.assets.find('model', this.collision_id);
-          this.collidermesh = collider;
-          if (collider.userData.loaded) {
-            //this.extractColliders(collider, true);
-            collider.userData.thing = this;
-            this.colliders.add(collider);
-          } else {
-            elation.events.add(collider, 'asset_load', elation.bind(this, function(ev) {  
+          var colliderasset = this.getAsset('model', collider_id);
+          if (colliderasset) {
+            var collider = colliderasset.getInstance();
+            this.collidermesh = collider;
+            if (collider.userData.loaded) {
+              this.extractColliders(collider, true);
               collider.userData.thing = this;
-              //this.extractColliders(collider, true);
-
-              //collider.bindPosition(this.position);
-              //collider.bindQuaternion(this.orientation);
-              //collider.bindScale(this.properties.scale);
-
-              collider.traverse(elation.bind(this, function(n) {
-                if (n.geometry) {
-                  n.geometry.computeVertexNormals();
-                  n.geometry.computeFaceNormals();
-                }
-                if (n.material) n.material = new THREE.MeshLambertMaterial({color: 0x999900, opacity: .2, transparent: true, emissive: 0x444400, alphaTest: .01, depthTest: false, depthWrite: false});
-                n.userData.thing = this;
-              }));
               this.colliders.add(collider);
+            } else {
+              elation.events.add(collider, 'asset_load', elation.bind(this, function(ev) {  
+                collider.userData.thing = this;
+                this.extractColliders(collider, true);
 
-            }) );
+                //collider.bindPosition(this.position);
+                //collider.bindQuaternion(this.orientation);
+                //collider.bindScale(this.properties.scale);
+
+                collider.traverse(elation.bind(this, function(n) {
+                  if (n.geometry) {
+                    n.geometry.computeVertexNormals();
+                    n.geometry.computeFaceNormals();
+                  }
+                  if (n.material) n.material = new THREE.MeshLambertMaterial({color: 0x999900, opacity: .2, transparent: true, emissive: 0x444400, alphaTest: .01, depthTest: false, depthWrite: false});
+                  n.userData.thing = this;
+                }));
+                this.colliders.add(collider);
+
+              }) );
+            }
           }
         }
       }
     }
     this.createObjectDOM = function() {
-      return;
-      if (this.properties.websurface_id) {
-        var websurface = this.properties.room.websurfaces[this.properties.websurface_id];
-        console.log('do a websurface: ' + this.properties.websurface_id, websurface);
+/*
+      if (this.websurface_id) {
+        var websurface = this.room.websurfaces[this.properties.websurface_id];
         if (websurface) {
           var width = websurface.width || 1024,
               height = websurface.height || 768;
 
           var iframe = elation.html.create('iframe');
-          iframe.src = 'http://www.github.com/';//websurface.src;
+          iframe.src = this.url;
           var div = elation.html.create('div');
           div.className = 'janusweb_websurface ';
           div.appendChild(iframe);
-
           div.style.width = width + 'px';
           div.style.height = height + 'px';
           iframe.style.width = width + 'px';
@@ -113,9 +145,22 @@ elation.require(['janusweb.janusbase'], function() {
           this.objects['3d'].add(obj);
         }
       }
+*/
     }
     this.updateMaterial = function() {
       this.assignTextures();
+    }
+    this.replaceWebsurfaceMaterial = function() {
+      var blankmaterial = new THREE.MeshBasicMaterial({
+        color: 0x000000,
+        opacity: 0,
+        transparent: true,
+        blending: THREE.NoBlending,
+        side: THREE.DoubleSide
+      });
+      this.objects['3d'].traverse(function(n) {
+        if (n.material) n.material = blankmaterial;
+      });
     }
     this.assignTextures = function() {
       //console.log('assign textures', this.name, this.objects['3d']);
@@ -128,20 +173,18 @@ elation.require(['janusweb.janusbase'], function() {
           side = this.sidemap[this.properties.cull_face];
 
       if (this.janusid) {
-        modelasset = elation.engine.assets.find('model', this.janusid, true);
+        modelasset = this.getAsset('model', this.janusid);
       }
       if (this.properties.image_id) {
-        texture = elation.engine.assets.find('image', this.image_id);
-        if (!texture) {
-          var asset = { assettype:'image', name:this.image_id, src: this.image_id, baseurl: this.baseurl }; 
-          elation.engine.assets.loadJSON([asset], this.baseurl); 
-          texture = elation.engine.assets.find('image', this.image_id);
+        textureasset = this.getAsset('image', this.image_id);
+        if (textureasset) {
+          texture = textureasset.getInstance();
+          //elation.events.add(texture, 'asset_load', elation.bind(this, this.assignTextures));
+          elation.events.add(texture, 'update', elation.bind(this, this.refresh));
         }
-        elation.events.add(texture, 'asset_load', elation.bind(this, this.assignTextures));
-        elation.events.add(texture, 'update', elation.bind(this, this.refresh));
       }
       if (this.properties.video_id) {
-        var videoasset = elation.engine.assets.find('video', this.properties.video_id, true);
+        var videoasset = this.getAsset('video', this.properties.video_id);
         if (videoasset) {
           texture = videoasset.getInstance();
           if (videoasset.sbs3d) {
@@ -159,6 +202,10 @@ elation.require(['janusweb.janusbase'], function() {
           elation.events.add(this, 'click', elation.bind(this, this.pauseVideo));
           this.videotexture = texture;
         }
+      }
+      if (this.properties.websurface_id) {
+        this.replaceWebsurfaceMaterial();
+        return;
       }
       if (this.properties.color !== this.defaultcolor) {
         color = this.properties.color;
@@ -193,7 +240,7 @@ elation.require(['janusweb.janusbase'], function() {
       if (!this.hasalpha) this.hasalpha = {};
       var hasalpha = this.hasalpha;
       var remove = [];
-      var cloneMaterial = false;
+      var cloneMaterial = true;//(texture !== false);
       this.objects['3d'].traverse(elation.bind(this, function(n) { 
         if (n.material) {
           var materials = [];
@@ -207,14 +254,19 @@ elation.require(['janusweb.janusbase'], function() {
                 materials.push(n.material.materials[i]);
               }
             }
-            n.material.materials = materials;
+            if (cloneMaterial) {
+              n.material = new THREE.MeshFaceMaterial(materials);
+            } else {
+              n.material.materials = materials;
+            }
           } else {
-/*
-            var m = this.copyMaterial(n.material);
-            materials.push(m); 
-            n.material = m;
-*/
-            materials.push(n.material);
+            if (cloneMaterial) {
+              var m = this.copyMaterial(n.material);
+              materials.push(m); 
+              n.material = m;
+            } else {
+              materials.push(n.material);
+            }
           }
 
           for (var i = 0; i < materials.length; i++) {
@@ -222,49 +274,22 @@ elation.require(['janusweb.janusbase'], function() {
             //m.envMap = scene.background;
             if (texture && texture.image) {
               m.map = texture; 
-            }
-            if (m.map && m.map.image) {
-/*
-              if (m.map.image instanceof HTMLCanvasElement) {
-                // FIXME - don't think this works
-                //hasalpha[m.map.image.src] = this.canvasHasAlpha(m.map.image);
-                if (hasalpha[m.map.image.src]) {
-                  m.transparent = true;
-                  m.alphaTest = 0.01;
-                  m.needsUpdate = true;
-                }
-              } else if (m.map.image.src && m.map.image.src.match(/\.(png|tga)$/)) {
-                // If the image is a PNG, check to see if it's got an alpha channel
-                // If it does, set the proper material parameters
-                elation.events.add(m.map.image, 'load', elation.bind(this, function(ev) {
-                  if (typeof hasalpha[ev.target.src] == 'undefined') {
-                    var canvas = document.createElement('canvas');
-                    canvas.width = m.map.image.width;
-                    canvas.height = m.map.image.height;
-                    var ctx = canvas.getContext('2d');
-                    ctx.drawImage(m.map.image, 0, 0);
-
-                    //hasalpha[ev.target.src] = this.canvasHasAlpha(canvas);
-                    m.map.image = canvas;
-                  }
-                  if (hasalpha[ev.target.src]) {
-                    m.transparent = true;
-                    m.alphaTest = 0.01;
-                    m.needsUpdate = true;
-                  }
-                }));
+            } else if (m.map) {
+              var imagesrc = m.map.sourceFile;
+              var asset = this.getAsset('image', imagesrc);
+              if (asset && asset.hasalpha) {
+                m.transparent = true;
+                m.alphaTest = 0.01;
               }
-*/
             }
             //m.roughness = 0.75;
             if (color) {
               m.color = color;
             }
-            if (side) {
-              m.side = side;
-            }
+            m.side = side;
             if (blend_src) m.blendSrc = blend_src;
             if (blend_dest) m.blendDst = blend_dest;
+            m.blending = THREE.CustomBlending;
             m.needsUpdate = true;
           }
         } else if (n instanceof THREE.Light) {
@@ -277,16 +302,24 @@ elation.require(['janusweb.janusbase'], function() {
       this.refresh();
     }
     this.copyMaterial = function(oldmat) {
-      //var m = (this.properties.lighting != false || oldmat.lightMap ? new THREE.MeshPhongMaterial() : new THREE.MeshBasicMaterial());
-      var m = new THREE.MeshPhongMaterial();
-      m.anisotropy = 16;
-      m.name = oldmat.name;
-      m.map = oldmat.map;
-      m.normalMap = oldmat.normalMap;
-      m.lightMap = oldmat.lightMap;
-      m.color.copy(oldmat.color);
-      m.transparent = oldmat.transparent;
-      m.alphaTest = oldmat.alphaTest;
+      if (oldmat instanceof THREE.MultiMaterial) {
+        var materials = [];
+        for (var i = 0; i < oldmat.materials.length; i++) {
+          materials.push(this.copyMaterial(oldmat.materials[i]));
+        }
+        var m = new THREE.MultiMaterial(materials);
+      } else {
+        var m = (this.properties.lighting != false ? new THREE.MeshPhongMaterial() : new THREE.MeshBasicMaterial());
+        //var m = new THREE.MeshBasicMaterial();
+        m.anisotropy = 16;
+        m.name = oldmat.name;
+        m.map = oldmat.map;
+        m.normalMap = oldmat.normalMap;
+        m.lightMap = oldmat.lightMap;
+        m.color.copy(oldmat.color);
+        m.transparent = oldmat.transparent;
+        m.alphaTest = oldmat.alphaTest;
+      }
 
       return m;
     }
@@ -301,34 +334,52 @@ elation.require(['janusweb.janusbase'], function() {
       }
     }
     this.start = function() {
-      if (this.properties.image_id) {
-        var texture = elation.engine.assets.find('image', this.properties.image_id);
-        console.log('start the image!', texture);
+      if (this.image_id) {
+        var textureasset = this.getAsset('image', this.image_id);
+        if (textureasset) {
+          var texture = textureasset.getInstance();
+          //console.log('start the image!', texture);
+        }
       }
-      if (this.properties.video_id) {
-        var texture = elation.engine.assets.find('video', this.properties.video_id);
+      if (this.video_id) {
+        var texture = elation.engine.assets.find('video', this.video_id);
         if (!texture.image.playing) {
           texture.image.play();
-          console.log('start the video!', texture);
+          //console.log('start the video!', texture);
         }
+      }
+      if (this.websurface_id) {
+        if (!this.websurface) {
+          this.websurface = this.spawn('januswebsurface', null, {janus: this.janus, room: this.room, websurface_id: this.websurface_id});
+          elation.events.add(this, 'mouseover', elation.bind(this.websurface, this.websurface.hover));
+          elation.events.add(this, 'mouseout', elation.bind(this.websurface, this.websurface.unhover));
+        }
+        this.websurface.start();
       }
     }
     this.stop = function() {
-      if (this.properties.image_id) {
-        var texture = elation.engine.assets.find('image', this.properties.image_id);
+      if (this.image_id) {
+        var texture = elation.engine.assets.find('image', this.image_id);
         console.log('stop the image!', texture);
       }
-      if (this.properties.video_id) {
-        var texture = elation.engine.assets.find('video', this.properties.video_id);
+      if (this.video_id) {
+        var texture = elation.engine.assets.find('video', this.video_id);
         texture.image.pause();
         console.log('stop the video!', texture);
+      }
+      if (this.websurface_id && this.websurface) {
+        this.websurface.stop();
       }
     }
     this.getProxyObject = function() {
       var proxy = elation.engine.things.janusobject.extendclass.getProxyObject.call(this);
       proxy._proxydefs = {
         id:  [ 'property', 'janusid'],
+        url:  [ 'property', 'url'],
+        collider_id:  [ 'property', 'collider_id'],
+        collider_scale:  [ 'property', 'collider_scale'],
         collision_id:  [ 'property', 'collision_id'],
+        collision_static:  [ 'property', 'collision_static'],
       };
       return proxy;
     }
