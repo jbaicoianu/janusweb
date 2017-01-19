32e98821eeadcb585777b60e227aa51ed8b0b5a3
James Baicoianu <james_github@baicoianu.com>
2017-01-18 22:11:26 -0800

    Added sharing app IDs, increase asset workers

--- scripts/config.js
+++ scripts/config.js
@@ -1,13 +1,6 @@
-/*
-elation.config.set('dependencies.protocol', 'https:');              // "http:" or "https:"
-elation.config.set('dependencies.host', 'janusweb.metacade.com');   // Hostname this release will live on
-elation.config.set('dependencies.rootdir', '/');                    // Directory this release will live in
-elation.config.set('dependencies.main', 'janusweb.js');             // The main script file for this release
-*/
-
 elation.config.set('janusweb.network.host', 'wss://presence.janusvr.com:5567');        // Default presence server
 elation.config.set('engine.assets.corsproxy', 'https://janusweb.lnq.to:8089/'); // CORS proxy URL
-elation.config.set('engine.assets.workers', 2); // CORS proxy URL
+elation.config.set('engine.assets.workers', 4); // Number of workers to use for asset parsing
 
 elation.config.set('janusweb.tracking.enabled', true);
 elation.config.set('janusweb.tracking.clientid', 'UA-49582649-2');
@@ -18,6 +11,10 @@ elation.config.set('dependencies.rootdir', '/');                    // Directory
 elation.config.set('dependencies.main', 'scripts/utils/elation.js');             // The main script file for this release
 
 elation.config.set('demohack.vive', false);
+
+elation.config.set('share.targets.imgur.clientid', '68bc9426b322db4');
+elation.config.set('share.targets.dropbox.clientid', '8u2gus0kinv8172');
+elation.config.set('share.targets.google.clientid', '374523350201-p566ctvssq49sa48aj2gistjuak3ci7k.apps.googleusercontent.com');
 
 // You probably don't want to edit past this line unless you know what you're doing
 // --------------------------------------------------------------------------------
