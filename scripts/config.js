286496ad34e45c2416d0cae6771b00e67f939ec4
James Baicoianu <james_github@baicoianu.com>
2017-01-22 00:36:21 -0800

    Share target and WebVR polyfill config

--- scripts/config.js
+++ scripts/config.js
@@ -5,7 +5,7 @@ elation.config.set('engine.assets.workers', 4); // Number of workers to use for 
 elation.config.set('janusweb.tracking.enabled', true);
 elation.config.set('janusweb.tracking.clientid', 'UA-49582649-2');
 
-elation.config.set('dependencies.protocol', 'http:');              // "http:" or "https:"
+elation.config.set('dependencies.protocol', 'https:');              // "http:" or "https:"
 elation.config.set('dependencies.host', 'bai.dev.supcrit.com');   // Hostname this release will live on
 elation.config.set('dependencies.rootdir', '/');                    // Directory this release will live in
 elation.config.set('dependencies.main', 'scripts/utils/elation.js');             // The main script file for this release
@@ -13,9 +13,13 @@ elation.config.set('dependencies.main', 'scripts/utils/elation.js');            
 elation.config.set('demohack.vive', false);
 
 elation.config.set('share.imagebase', '/media/images/share/');
-elation.config.set('share.targets.imgur.clientid', '68bc9426b322db4');
+elation.config.set('share.targets.imgur.clientid', '68bc9426b322db4'); // web.janusvr.com
+//elation.config.set('share.targets.imgur.clientid', '96d8f6e2515953a');// bai.dev
 elation.config.set('share.targets.dropbox.clientid', '8u2gus0kinv8172');
-elation.config.set('share.targets.google.clientid', '374523350201-p566ctvssq49sa48aj2gistjuak3ci7k.apps.googleusercontent.com');
+elation.config.set('share.targets.googledrive.clientid', '374523350201-p566ctvssq49sa48aj2gistjuak3ci7k.apps.googleusercontent.com');
+elation.config.set('share.targets.yahoo.clientid', '374523350201-p566ctvssq49sa48aj2gistjuak3ci7k.apps.googleusercontent.com');
+elation.config.set('share.targets.facebook.clientid', '1197654320349894');
+elation.config.set('share.targets.file.enabled', true);
 
 // You probably don't want to edit past this line unless you know what you're doing
 // --------------------------------------------------------------------------------
@@ -24,3 +28,10 @@ elation.config.set('share.targets.google.clientid', '374523350201-p566ctvssq49sa
 elation.config.set('dependencies.path', elation.config.get('dependencies.protocol') + '//' + elation.config.get('dependencies.host') + elation.config.get('dependencies.rootdir'));
 elation.config.set('janusweb.datapath', elation.config.get('dependencies.path') + 'media/janusweb/');
 elation.config.set('engine.assets.font.path', elation.config.get('janusweb.datapath') + 'fonts/');
+
+WebVRConfig = {
+  FORCE_ENABLE_VR: false,
+  MOUSE_KEYBOARD_CONTROLS_DISABLED: true,
+  DEFER_INITIALIZATION: false
+};
+
