e1458ba8eb9c741bc9ebc733ea2fd51e2684034c
James Baicoianu <james_github@baicoianu.com>
2017-07-20 23:28:39 -0700

    Massive scripting improvements, removed cursor logic

--- scripts/janusbase.js
+++ scripts/janusbase.js
@@ -14,11 +14,14 @@ elation.require(['engine.things.generic', 'utils.template'], function() {
         room:     { type: 'object' },
         janus:    { type: 'object' },
         js_id:    { type: 'string' },
-        color:    { type: 'color', default: this.defaultcolor, set: this.updateMaterial },
+        color:    { type: 'color', default: this.defaultcolor, set: this.updateColor },
+        opacity:  { type: 'float', default: 1.0, set: this.updateOpacity },
         fwd:      { type: 'vector3', default: new THREE.Vector3(0,0,1), set: this.pushFrameUpdate },
         xdir:     { type: 'vector3', default: new THREE.Vector3(1,0,0), set: this.pushFrameUpdate },
         ydir:     { type: 'vector3', default: new THREE.Vector3(0,1,0), set: this.pushFrameUpdate },
         zdir:     { type: 'vector3', default: new THREE.Vector3(0,0,1), set: this.pushFrameUpdate },
+        rotation: { type: 'euler', default: new THREE.Euler(0,0,0), set: this.pushFrameUpdate },
+        rotation_order: { type: 'string', default: 'XYZ', set: this.pushFrameUpdate },
         lighting: { type: 'boolean', default: true },
         sync:     { type: 'boolean', default: false },
         rotate_axis: { type: 'string', default: '0 1 0' },
@@ -27,9 +30,11 @@ elation.require(['engine.things.generic', 'utils.template'], function() {
         anim_id: { type: 'string' },
         anim_transition_time: { type: 'float', default: .2 },
       });
+
+      this.eventlistenerproxies = {};
       //if (this.col) this.color = this.col;
       elation.events.add(this.room, 'janusweb_script_frame_end', elation.bind(this, this.handleFrameUpdates));
-      elation.events.add(this, 'mouseover,mouseout', elation.bind(this, this.updatePlayerCursor));
+      this.colorIsDefault = true;
 
       if (this.onclick) {
         if (elation.utils.isString(this.onclick)) {
@@ -41,6 +46,18 @@ elation.require(['engine.things.generic', 'utils.template'], function() {
           elation.events.add(this, 'click', elation.bind(this, this.onclick));
         }
       }
+    }
+    this.updateColor = function() {
+      if (this.properties.color === this.defaultcolor) {
+        if (this.color.r != 1 || this.color.g != 1 || this.color.b != 1) {
+          this.properties.color = this.properties.color.clone();
+          this.defaultcolor.setRGB(1,1,1);
+          this.colorIsDefault = false;
+        }
+      }
+    }
+    this.updateOpacity = function() {
+      this.setOpacity(this.opacity);
     }
     this.createForces = function() {
       elation.events.add(this.objects.dynamics, 'physics_collide', elation.bind(this, this.handleCollision));
@@ -107,9 +124,11 @@ elation.require(['engine.things.generic', 'utils.template'], function() {
       var xml = elation.template.get('janusweb.edit.object', objdef);
       return xml;
     }
-    this.getProxyObject = function() {
+    this.getProxyObject = function(classdef) {
       if (!this._proxyobject) {
         this._proxyobject = new elation.proxy(this, {
+          parent:   ['accessor', 'parent.getProxyObject'],
+          children: ['accessor', 'getChildProxies'],
           js_id:    ['property', 'properties.js_id'],
           pos:      ['property', 'position'],
           vel:      ['property', 'velocity'],
@@ -117,21 +136,74 @@ elation.require(['engine.things.generic', 'utils.template'], function() {
           mass:     ['property', 'mass'],
           scale:    ['property', 'scale'],
           col:      ['property', 'color'],
+          opacity:  ['property', 'opacity'],
           fwd:      ['property', 'zdir'],
           xdir:     ['property', 'xdir'],
           ydir:     ['property', 'ydir'],
           zdir:     ['property', 'zdir'],
+          rotation: ['property', 'rotation'],
+          rotation_order: ['property', 'rotation_order'],
           sync:     ['property', 'sync'],
           visible:  ['property', 'visible'],
-          children: ['property', 'jschildren'],
 
-          oncollision: ['callback', 'collision'],
-          //onclick:     ['callback', 'click'],
+          onupdate:     ['callback', 'update'],
+          oncollision:  ['callback', 'collision'],
+          onmouseover:  ['callback', 'mouseover'],
+          onmouseout:   ['callback', 'mouseout'],
+          onmousemove:  ['callback', 'mousemove'],
+          onmousedown:  ['callback', 'mousedown'],
+          onmouseup:    ['callback', 'mouseup'],
+          onclick:      ['callback', 'click'],
+          ontouchstart: ['callback', 'touchstart'],
+          ontouchmove:  ['callback', 'touchmove'],
+          ontouchend:   ['callback', 'touchend'],
+          ondragover:   ['callback', 'dragover'],
+          ondrag:       ['callback', 'drag'],
+          ondragenter:  ['callback', 'dragenter'],
+          ondragleave:  ['callback', 'dragleave'],
+          ondragstart:  ['callback', 'dragstart'],
+          ondragend:    ['callback', 'dragend'],
+          ondrop:       ['callback', 'drop'],
 
-          appendChild: ['function', 'appendChild']
+          createObject:        ['function', 'createObject'],
+          appendChild:         ['function', 'appendChild'],
+          addEventListener:    ['function', 'addEventListenerProxy'],
+          removeEventListener: ['function', 'removeEventListenerProxy'],
+          localToWorld:        ['function', 'localToWorld'],
+          worldToLocal:        ['function', 'worldToLocal'],
+          distanceTo:          ['function', 'distanceTo'],
+          addForce:            ['function', 'addForce'],
+          removeForce:         ['function', 'removeForce'],
+          die:                 ['function', 'die'],
+          executeCallback:     ['function', 'executeCallback']
         });
+
+        if (classdef) {
+          var propertydefs = {},
+              proxydefs = {};
+          for (var k in classdef.class) {
+            var v = classdef.class[k];
+            var proxytype = 'property';
+            if (typeof v == 'function') {
+              proxytype = 'function';
+              this._proxyobject[k] = elation.bind(this._proxyobject, v);
+            } else {
+              propertydefs[k] = {type: 'object', default: v };
+            }
+            proxydefs[k] = [proxytype, k];
+          }
+          this.defineProperties(propertydefs);
+          this._proxyobject._proxydefs = proxydefs;
+        }
       }
       return this._proxyobject;
+    }
+    this.getChildProxies = function() {
+      var childproxies = [];
+      for (var k in this.children) {
+        childproxies.push(this.children[k]._proxyobject);
+      }
+      return childproxies;
     }
     this.getAsset = function(type, id) {
       if (!this.assets[type]) {
@@ -159,7 +231,9 @@ elation.require(['engine.things.generic', 'utils.template'], function() {
 //console.log('frame update!', key, value);
       this.frameupdates[key] = value;
     }
-    this.handleFrameUpdates = function() {
+    this.handleFrameUpdates = function(ev) {
+      this.dispatchEvent({type: 'update', data: ev.data});
+
       var updatenames = Object.keys(this.frameupdates);
       var xdir = this.properties.xdir,
           ydir = this.properties.ydir,
@@ -172,6 +246,8 @@ elation.require(['engine.things.generic', 'utils.template'], function() {
       );
       if (updatenames.length > 0 || diff) {
         var updates = this.frameupdates;
+        if ('rotation' in updates) {
+        }
         if ('fwd' in updates) {
           this.properties.zdir.copy(this.fwd);
           updates.zdir = this.properties.zdir;
@@ -232,11 +308,15 @@ elation.require(['engine.things.generic', 'utils.template'], function() {
 
           quat.setFromRotationMatrix(mat4);
           this.properties.orientation.copy(quat);
+          this.properties.rotation.setFromRotationMatrix(mat4);
         } else if (this.objects['3d']) {
           //this.objects['3d'].matrix.extractBasis(this.properties.xdir, this.properties.ydir, this.properties.zdir);
         }
       };
     })();
+    this.createObject = function(type, args) {
+      return room.createObject(type, args, this);
+    }
     this.appendChild = function(obj) {
       var proxyobj = obj
       if (elation.utils.isString(obj)) {
@@ -294,20 +374,19 @@ console.error('dunno what this is', other);
       }
     }
     this.setOpacity = function(opacity) {
-      this.objects['3d'].traverse(function(n) {
-        if (n.material) {
-          n.material.opacity = opacity;
-        }
-      });
-    }
-    this.updatePlayerCursor = function(ev) {
-      var vrdisplay = this.engine.systems.render.views.main.vrdisplay;
-      if (ev.type == 'mouseover' && (this.onclick || elation.events.hasEventListener(this, 'click'))) {
-        this.engine.client.player.cursor_style = 'pointer';
-      } else if (this.engine.systems.controls.pointerLockActive || (vrdisplay && vrdisplay.isPresenting)) {
-        this.engine.client.player.cursor_style = 'crosshair';
-      } else {
-        this.engine.client.player.cursor_style = 'default';
+      if (this.objects['3d']) {
+        this.objects['3d'].traverse(function(n) {
+          if (n.material) {
+            var m = (elation.utils.isArray(n.material) ? n.material : [n.material]);
+            for (var i = 0; i < m.length; i++) {
+              m[i].opacity = opacity;
+              m[i].transparent = (opacity < 1);
+              if (m[i].transparent) {
+                m[i].alphaTest = 0.02;
+              }
+            }
+          }
+        });
       }
     }
     this.setAnimation = function(anim_id) {
@@ -325,6 +404,45 @@ console.error('dunno what this is', other);
           this.activeanimation = action;
         }
         this.anim_id = anim_id;
+      }
+    }
+    this.dispatchEvent = function(event) {
+      if (!event.element) event.element = this;
+      return elation.events.fire(event);
+    }
+    this.addEventListenerProxy = function(name, handler, bubble) {
+      var eventobj = {
+        target: handler,
+        fn: function(ev) {
+          var proxyev = elation.events.clone(ev, {
+            target: ev.target.getProxyObject(),
+          });
+          // Bind stopPropagation and preventDefault functions to the real event
+          proxyev.stopPropagation = elation.bind(ev, ev.stopPropagation),
+          proxyev.preventDefault = elation.bind(ev, ev.preventDefault),
+          handler(proxyev);
+        }
+      };
+      if (!this.eventlistenerproxies[name]) this.eventlistenerproxies[name] = [];
+      this.eventlistenerproxies[name].push(eventobj);
+
+      elation.events.add(this, name, eventobj.fn, bubble);
+    }
+    this.removeEventListenerProxy = function(name, handler, bubble) {
+      if (this.eventlistenerproxies[name]) {
+        for (var i = 0; i < this.eventlistenerproxies[name].length; i++) {
+          var evproxy = this.eventlistenerproxies[name][i];
+          if (evproxy.target === handler) {
+            elation.events.remove(this, name, evproxy.fn, bubble);
+          }
+        }
+      }
+    }
+    this.executeCallback = function(callback, args) {
+      if (callback instanceof Function) {
+        callback(args);
+      } else if (elation.utils.isString(callback)) {
+        eval(callback);
       }
     }
   }, elation.engine.things.generic);
