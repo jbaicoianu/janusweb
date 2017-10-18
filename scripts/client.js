301c48d2e09d779b7a9175b08c65e45fa4c5e1df
James Baicoianu <james_github@baicoianu.com>
2017-10-17 17:24:39 -0700

    Added new single-line client custom elements

--- scripts/client.js
+++ scripts/client.js
@@ -1,4 +1,4 @@
-elation.require(['engine.engine', 'engine.assets', 'engine.things.light_ambient', 'engine.things.light_directional', 'engine.things.light_point', 'janusweb.janusweb', 'janusweb.chat', 'janusweb.janusplayer', 'janusweb.ui', 'janusweb.configuration'], function() {
+elation.require(['engine.engine', 'engine.assets', 'engine.things.light_ambient', 'engine.things.light_directional', 'engine.things.light_point', 'janusweb.janusweb', 'janusweb.chat', 'janusweb.janusplayer', 'janusweb.ui', 'janusweb.configuration', 'janusweb.external.document-register-element'], function() {
 
   // If getCurrentScript returns non-null here, then it means we're in release mode
   var clientScript = elation.utils.getCurrentScript();
@@ -233,4 +233,89 @@ elation.require(['engine.engine', 'engine.assets', 'engine.things.light_ambient'
       this.janusweb.registerElement(tagname, classobj, extendclass);
     }
   }, elation.engine.client);
+
+  if (typeof customElements != 'undefined') {
+    elation.extend('janusweb.viewer.base', class extends HTMLElement {
+      get src() {
+        return this.getAttribute('src');
+      }
+      set src(src) {
+        return this.setAttribute('src', src);
+      }
+      get width() {
+        return this.getAttribute('width') || 640;
+      }
+      set width(width) {
+        return this.setAttribute('width', width);
+      }
+      get height() {
+        return this.getAttribute('height') || 480;
+      }
+      set height(height) {
+        return this.setAttribute('height', width);
+      }
+      connectedCallback() {
+        this.init();
+      }
+      init() {
+        var iframe = document.createElement('iframe');
+        iframe.width = this.width;
+        iframe.height = this.height;
+        this.appendChild(iframe);
+        var content = iframe.contentWindow || iframe.contentDocument.document || iframe.contentDocument;
+        content.document.open();
+        content.document.write('<html><body style="overflow: hidden">');
+        content.document.write('<script src="' + clientScript.src + '"></script>');
+        content.document.write('<script>elation.janusweb.init(' + JSON.stringify(this.getClientArgs()) + ')</script>');
+        content.document.write('</body></html>');
+        content.document.close();
+      }
+      getClientArgs() {
+        var args = {
+          url: this.getRoomURL(),
+          resolution: this.width + 'x' + this.height,
+          shownavigation: false,
+        };
+        return args;
+      }
+      getRoomURL() {
+        return this.src || document.location.href;
+      }
+    });
+    customElements.define('janus-viewer', elation.janusweb.viewer.base);
+
+    elation.extend('janusweb.viewer.image360', class extends elation.janusweb.viewer.base {
+      getRoomURL() {
+        return 'data:text/html,' + encodeURIComponent('<fireboxroom><assets><assetimage id="image360" src="' + this.src + '"/></assets><room skybox_left_id="black" skybox_right_id="black" skybox_up_id="black" skybox_down_id="black" skybox_front_id="black" skybox_back_id="black"><object id="sphere" scale="-500 500 500" image_id="image360" lighting="false" /></room></fireboxroom>');
+      }
+    });
+    customElements.define('janus-viewer-image360', elation.janusweb.viewer.image360);
+
+    elation.extend('janusweb.viewer.video', class extends elation.janusweb.viewer.base {
+      get loop() {
+        return this.getAttribute('loop');
+      }
+      set loop(loop) {
+        return this.setAttribute('loop', loop);
+      }
+      getRoomURL() {
+        return 'data:text/html,' + encodeURIComponent('<fireboxroom><assets><assetvideo id="video" src="' + this.src + '" auto_play="true" loop="' + (this.loop ? 'true' : 'false') + '" /></assets><room use_local_asset="room1" skybox_left_id="black" skybox_right_id="black" skybox_up_id="black" skybox_down_id="black" skybox_front_id="black" skybox_back_id="black"><Video video_id="video" pos="0 2.5 -5" scale="4 2.5 .1" lighting="false" /></room></fireboxroom>');
+      }
+    });
+    customElements.define('janus-viewer-video', elation.janusweb.viewer.video);
+
+    elation.extend('janusweb.viewer.video360', class extends elation.janusweb.viewer.video {
+      getRoomURL() {
+        return 'data:text/html,' + encodeURIComponent('<fireboxroom><assets><assetvideo id="video360" src="' + this.src + '" auto_play="true" loop="' + (this.loop ? 'true' : 'false') + '" /></assets><room skybox_left_id="black" skybox_right_id="black" skybox_up_id="black" skybox_down_id="black" skybox_front_id="black" skybox_back_id="black"><object id="sphere" scale="-500 500 500" video_id="video360" lighting="false" /></room></fireboxroom>');
+      }
+    });
+    customElements.define('janus-viewer-video360', elation.janusweb.viewer.video360);
+
+    elation.extend('janusweb.viewer.model', class extends elation.janusweb.viewer.base {
+      getRoomURL() {
+        return 'data:text/html,' + encodeURIComponent('<fireboxroom><assets><assetobject id="model" src="' + this.src + '"/></assets><room skybox_left_id="black" skybox_right_id="black" skybox_up_id="black" skybox_down_id="black" skybox_front_id="black" skybox_back_id="black"><object id="model" lighting="true" pos="0 0 -5" rotate_deg_per_sec="10" /></room></fireboxroom>');
+      }
+    });
+    customElements.define('janus-viewer-model', elation.janusweb.viewer.model);
+  }
 });
