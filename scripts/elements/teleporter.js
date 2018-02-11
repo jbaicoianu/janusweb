11aabf0b1a153134a65410b66114a93f9100a83b
James Baicoianu <james_github@baicoianu.com>
2018-02-11 20:21:22 +0000

    Fixed teleporter stickiness

--- scripts/elements/teleporter.js
+++ scripts/elements/teleporter.js
@@ -3,7 +3,7 @@ elation.component.add('janusweb.elements.teleporter', {
     longpresstime: 250,
     deadzone: 5,
 
-    createChildren() {
+    create() {
       this.marker = this.createObject('Object', {
         id: 'pipe',
         col: '#009',
@@ -54,6 +54,7 @@ elation.component.add('janusweb.elements.teleporter', {
     handleMouseDown(ev) {
       this.longpresstimer = setTimeout(this.enableCursor, this.longpresstime);
       this.mousediff = [0,0];
+      this.active = false;
     },
     handleMouseMove(ev) {
       if (this.longpresstimer) {
