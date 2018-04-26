1d299616c0521df1e06fa72372ce67078c631039
James Baicoianu <james_github@baicoianu.com>
2018-04-26 03:32:03 -0700

    Skip proxy for localhost urls

--- scripts/room.js
+++ scripts/room.js
@@ -377,7 +377,7 @@ elation.require([
       if (fullurl[0] == '/' && fullurl[1] != '/') fullurl = this.baseurl + fullurl;
       if (!fullurl.match(/^https?:/) && !fullurl.match(/^\/\//)) {
         fullurl = self.location.origin + fullurl;
-      } else {
+      } else if (!fullurl.match(/^https?:\/\/(localhost|127\.0\.0\.1)/)) {
         fullurl = proxyurl + fullurl;
       }
 
