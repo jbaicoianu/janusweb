e51deaeff1f94fb5e0fd976395ff07ca8fad7c4c
James Baicoianu <james_github@baicoianu.com>
2019-03-30 02:24:52 -0700

    Use custom itemcomponent for inventory to allow for advanced logic

--- media/assets/webui/apps/inventory/inventory.js
+++ media/assets/webui/apps/inventory/inventory.js
@@ -2,4 +2,22 @@ elation.elements.define('janus.ui.inventory', class extends elation.elements.bas
   create() {
     this.innerHTML = elation.template.get('janus.ui.inventory');
   }
+  addSource(name, collection) {
+  }
+});
+elation.elements.define('janus.ui.inventory.item.poly', class extends elation.elements.ui.item {
+  create() {
+    let item = this.value;
+
+    for (let i = 0; i < item.formats.length; i++) {
+      if (item.formats[i].formatType == 'GLTF2') {
+        this.model = item.formats[i];
+      }
+    }
+    if (!this.model) {
+      this.model = item.formats[0];
+    }
+
+    let elements = elation.elements.fromTemplate('janus.ui.inventory.item.poly', this);
+  }
 });
