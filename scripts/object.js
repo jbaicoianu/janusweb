fb5349b554e1c00788983fdbd93fa007d605d363
James Baicoianu <james_github@baicoianu.com>
2019-07-24 15:51:51 -0700

    Removed debug log

--- scripts/object.js
+++ scripts/object.js
@@ -829,7 +829,7 @@ elation.require(['janusweb.janusbase', 'janusweb.websurface'], function() {
       elation.engine.things.janusobject.extendclass.stop.call(this);
       if (this.image_id) {
         var texture = elation.engine.assets.find('image', this.image_id);
-        console.log('stop the image!', texture);
+        //console.log('stop the image!', texture);
       }
       if (this.video_id) {
         //var texture = elation.engine.assets.find('video', this.video_id);
