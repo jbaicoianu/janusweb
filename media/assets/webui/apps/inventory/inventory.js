63095f515bf4d148b28440e4b2ffa583826de819
James Baicoianu <james_github@baicoianu.com>
2020-03-31 13:18:01 -0700

    Inventory draggable item fixes

--- media/assets/webui/apps/inventory/inventory.js
+++ media/assets/webui/apps/inventory/inventory.js
@@ -1,8 +1,27 @@
 elation.elements.define('janus.ui.inventory', class extends elation.elements.base {
   create() {
-    this.innerHTML = elation.template.get('janus.ui.inventory');
+    //this.innerHTML = elation.template.get('janus.ui.inventory');
+    let elements = elation.elements.fromTemplate('janus.ui.inventory', this);
+
+    console.log('INVENTORY ELEMENTS', elements);
+    if (elements.primitives) {
+      elation.events.add(elements.primitives, 'select', (ev) => this.handlePrimitiveSelect(ev));
+      elation.events.add(elements.polyobjects, 'select', (ev) => this.handleAssetSelect(ev));
+    }
   }
   addSource(name, collection) {
+  }
+  handlePrimitiveSelect(ev) {
+    let item = ev.data;
+    elation.events.fire({type: 'assetselect', data: item.url, element: this});
+    ev.preventDefault();
+    ev.stopPropagation();
+  }
+  handleAssetSelect(ev) {
+    let item = ev.data;
+    elation.events.fire({type: 'assetselect', data: item.url, element: this});
+    ev.preventDefault();
+    ev.stopPropagation();
   }
 });
 elation.elements.define('janus.ui.inventory.item.poly', class extends elation.elements.ui.item {
@@ -19,5 +38,6 @@ elation.elements.define('janus.ui.inventory.item.poly', class extends elation.el
     }
 
     let elements = elation.elements.fromTemplate('janus.ui.inventory.item.poly', this);
+    elation.events.add(this, 'click', (ev) => this.click(ev));
   }
 });
