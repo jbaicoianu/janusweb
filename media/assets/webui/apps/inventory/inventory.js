4a5b288ce4efd54a5bc2d08a1964909d660fcb3e
James Baicoianu <james_github@baicoianu.com>
2020-08-04 00:54:42 -0700

    Added room assets to inventory

--- media/assets/webui/apps/inventory/inventory.js
+++ media/assets/webui/apps/inventory/inventory.js
@@ -41,3 +41,42 @@ elation.elements.define('janus.ui.inventory.item.poly', class extends elation.el
     elation.events.add(this, 'click', (ev) => this.click(ev));
   }
 });
+elation.elements.define('janus.ui.inventory.roomassets', class extends elation.elements.base {
+  create() {
+    this.elements = elation.elements.fromTemplate('janus.ui.inventory.roomassets', this);
+  }
+});
+elation.elements.define('janus.ui.inventory.roomassets.list', class extends elation.elements.base {
+  constructor() {
+    super();
+    this.defineAttribute('assettype', { type: 'string' });
+  }
+  create() {
+    this.list = elation.elements.create('ui-grid', {
+      append: this,
+      class: 'models',
+      selectable: 1,
+      draggable: 1,
+      itemcomponent: 'janus.ui.inventory.item.roomasset',
+    });
+    this.updateList();
+    elation.events.add(janus._target, 'room_change', (ev) => this.updateList());
+  }
+  updateList() {
+    this.list.clear();
+    this.list.innerHTML = '';
+    this.list.setItems(this.getAssetList());
+  }
+  getAssetList() {
+    if (room.assetpack.assetmap[this.assettype]) {
+      return room.assetpack.assetmap[this.assettype];
+    }
+    return {};
+  }
+});
+elation.elements.define('janus.ui.inventory.item.roomasset', class extends elation.elements.ui.item {
+  create() {
+    let elements = elation.elements.fromTemplate('janus.ui.inventory.item.roomasset', this);
+    elation.events.add(this, 'click', (ev) => this.click(ev));
+  }
+});
