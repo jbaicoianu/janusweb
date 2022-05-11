6e717210820b77a7c178f73b588a4219f788629f
James Baicoianu <james_github@baicoianu.com>
2022-05-10 23:00:46 -0700

    Upgrade outline element to support skinned meshes

--- scripts/elements/outliner.js
+++ scripts/elements/outliner.js
@@ -6,28 +6,44 @@ elation.require(['janusweb.janusbase'], function() {
         'outline': { type: 'float', default: 2, set: this.updateOutlineSize },
       });
       this.meshes = [];
-      // Shader used to outline the seleced object.  Based on https://www.videopoetics.com/tutorials/pixel-perfect-outline-shaders-unity/
+      this.skinnedmeshes = [];
+      // Shader used to outline the selected object.  Based on https://www.videopoetics.com/tutorials/pixel-perfect-outline-shaders-unity/
+      // Thanks to @donrmccurdy for the example of how to include skinning in a custom shader https://github.com/mrdoob/three.js/issues/4800#issuecomment-580901344
       this.outlinematerial = new THREE.ShaderMaterial({
-        vertexShader: [
-            "uniform float offset;",
-            "uniform vec2 screenSize;",
-            "void main() {",
-                //"vec4 pos = modelViewMatrix * vec4( position + normal * offset, 1.0 );",
-                //"gl_Position = projectionMatrix * pos;",
-                "vec4 pos = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-                "vec3 clipNormal = mat3(projectionMatrix) * (mat3(modelViewMatrix) * normal);",
-                "pos.xy += normalize(clipNormal.xy) / screenSize * offset * pos.w * 2.0;",
-                "gl_Position = pos;",
-            "}"
-        ].join("\n"),
+        vertexShader: `
+            uniform float offset;
+            uniform vec2 screenSize;
 
-        fragmentShader: [
-            "uniform vec3 color;",
-            "uniform float opacity;",
-            "void main(){",
-                "gl_FragColor = vec4( color, opacity );",
-            "}"
-        ].join("\n"),
+            ${THREE.ShaderChunk.skinning_pars_vertex}
+
+            void main() {
+                mat4 modelViewProjectionMatrix = projectionMatrix * modelViewMatrix;
+
+                // If USE_SKINNING is set, this will transform our vertices by any skinned mesh attributes
+                // Otherwise, it'll just use our regular vertex positions
+                ${THREE.ShaderChunk.beginnormal_vertex}
+                ${THREE.ShaderChunk.skinbase_vertex}
+                ${THREE.ShaderChunk.skinnormal_vertex}
+                vec3 transformed = vec3( position );
+                ${THREE.ShaderChunk.skinning_vertex}
+
+                // Transform our new vertex position into clip space
+                vec4 pos = modelViewProjectionMatrix * vec4(transformed, 1.0);
+
+                // Offset our vertices along the normal direction, scaled to the specified pixel offset in screen space
+                vec3 clipNormal = mat3(projectionMatrix) * (mat3(modelViewMatrix) * normal);
+                pos.xy += normalize(clipNormal.xy) / screenSize * offset * pos.w * 2.0;
+                gl_Position = pos;
+            }
+        `,
+
+        fragmentShader: `
+            uniform vec3 color;
+            uniform float opacity;
+            void main(){
+                gl_FragColor = vec4( color, opacity );
+            }
+        `,
 
         uniforms: {
           offset: { type: "f", value: this.outline },
@@ -46,42 +62,63 @@ elation.require(['janusweb.janusbase'], function() {
       if (this.target) this.updateTarget();
     },
     select(object) {
-      let i = 0;
       this.selected = object;
-      let objmeshes = [];
+      let objmeshes = [], skinnedmeshes = [];
       object.traverseObjects(n => {
         if (n instanceof THREE.Mesh && n.material !== this.outlinematerial) {
-          objmeshes.push(n);
+          if (n.isSkinnedMesh)
+            skinnedmeshes.push(n);
+          else
+            objmeshes.push(n);
         }
       });
-      for (; i < objmeshes.length; i++) {
-        let n = objmeshes[i],
-            m = this.meshes[i];
-          if (!m) {
-            m = this.meshes[i] = new THREE.Mesh(n.geometry, this.outlinematerial);
-            m.matrixAutoUpdate = false;
-          } else {
-            m.geometry = n.geometry;
-            m.material = this.outlinematerial;
-          }
-          m.matrix.copy(n.matrix);
-          n.parent.add(m);
+      // Create and update regular Meshes
+      let meshIdx = 0;
+      for (; meshIdx < objmeshes.length; meshIdx++) {
+        let n = objmeshes[meshIdx],
+            m = this.getMesh(n, this.meshes[meshIdx]);
+        this.meshes[meshIdx] = m;
+        n.parent.add(m);
       }
-      if (i < this.meshes.length) {
-        for (; i < this.meshes.length; i++) {
-          this.meshes[i].geometry = null;
-          if (this.meshes[i].parent) {
-            this.meshes[i].parent.remove(this.meshes[i]);
+      // Hide all unused Meshes
+      if (meshIdx < this.meshes.length) {
+        for (; meshIdx < this.meshes.length; meshIdx++) {
+          this.meshes[meshIdx].geometry = null;
+          if (this.meshes[meshIdx].parent) {
+            this.meshes[meshIdx].parent.remove(this.meshes[meshIdx]);
+          }
+        }
+      }
+      // Create and update SkinnedMeshes
+      let skinnedMeshIdx = 0;
+      for (; skinnedMeshIdx < skinnedmeshes.length; skinnedMeshIdx++) {
+        let n = skinnedmeshes[skinnedMeshIdx],
+            m = this.getSkinnedMesh(n, this.skinnedmeshes[skinnedMeshIdx]);
+        this.skinnedmeshes[skinnedMeshIdx] = m;
+        n.parent.add(m);
+      }
+      // Hide all unused Meshes
+      if (skinnedMeshIdx < this.skinnedmeshes.length) {
+        for (; skinnedMeshIdx < this.skinnedmeshes.length; skinnedMeshIdx++) {
+          this.skinnedmeshes[skinnedMeshIdx].geometry = null;
+          if (this.skinnedmeshes[skinnedMeshIdx].parent) {
+            this.skinnedmeshes[skinnedMeshIdx].parent.remove(this.skinnedmeshes[skinnedMeshIdx]);
           }
         }
       }
       this.refresh();
     },
     deselect() {
-      for (let i = 0; i < this.meshes.length; i++) {
-        this.meshes[i].geometry = null;
-        if (this.meshes[i].parent) {
-          this.meshes[i].parent.remove(this.meshes[i]);
+      for (let meshIdx = 0; meshIdx < this.meshes.length; meshIdx++) {
+        this.meshes[meshIdx].geometry = null;
+        if (this.meshes[meshIdx].parent) {
+          this.meshes[meshIdx].parent.remove(this.meshes[meshIdx]);
+        }
+      }
+      for (let skinnedMeshIdx = 0; skinnedMeshIdx < this.skinnedmeshes.length; skinnedMeshIdx++) {
+        this.skinnedmeshes[skinnedMeshIdx].geometry = null;
+        if (this.skinnedmeshes[skinnedMeshIdx].parent) {
+          this.skinnedmeshes[skinnedMeshIdx].parent.remove(this.skinnedmeshes[skinnedMeshIdx]);
         }
       }
       this.refresh();
@@ -111,6 +148,29 @@ elation.require(['janusweb.janusbase'], function() {
         }
       }
     },
+    getMesh(n, m) {
+      if (!m) {
+        m = new THREE.Mesh(n.geometry, this.outlinematerial);
+        m.matrixAutoUpdate = false;
+      } else {
+        m.geometry = n.geometry;
+        m.material = this.outlinematerial;
+      }
+      m.matrix.copy(n.matrix);
+      return m;
+    },
+    getSkinnedMesh(n, m) {
+      if (!m) {
+        m = new THREE.SkinnedMesh(n.geometry, this.outlinematerial);
+        m.skeleton = n.skeleton;
+        m.matrixAutoUpdate = false;
+      } else {
+        m.geometry = n.geometry;
+        m.material = this.outlinematerial;
+      }
+      m.matrix.copy(n.matrix);
+      return m;
+    }
   }, elation.janusweb.janusbase);
 
 });
