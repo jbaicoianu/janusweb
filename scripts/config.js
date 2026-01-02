a189bb9e990f9abbeb6f90f85d340e36582088cd
Leon van Kammen <leonvankammen@gmail.com>
2026-01-02 17:32:15 +0100

    added optional roundrobin cors proxies

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,16 @@
 elation.config.set('janusweb.network.host', 'wss://presence.janusxr.org:5567');        // Default presence server
-elation.config.set('engine.assets.corsproxy', 'https://p.janusxr.org/'); // CORS proxy URL
+
+proxies = [
+  'https://p.janusxr.org/',
+  'https://cors.xrforge.isvery.ninja/'
+]
+elation.config.set('engine.assets.corsproxy',   proxies[0]); // CORS proxy URL
+elation.config.set('engine.assets.corsproxies', []        ); // rotate **online** proxies only
+proxies.map( (url) => 
+  fetch(url,{method:'HEAD'})
+  .then( (res) => res.ok && elation.config.get("engine.assets.corsproxies").push(url) )
+)
+
 elation.config.set('engine.assets.workers', 'auto'); // Number of workers to use for asset parsing
 //elation.config.set('engine.assets.image.maxsize', 16384); 
 elation.config.set('engine.assets.image.anisotropy', 16); 
