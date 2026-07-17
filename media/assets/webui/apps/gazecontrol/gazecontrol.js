32f343843a6cc6fb4d8ee0d4247e9582856ab66d
Leon van Kammen <leonvankammen@gmail.com>
2026-07-17 13:17:24 +0200

    added gazecontrol

--- media/assets/webui/apps/gazecontrol/gazecontrol.js
+++ media/assets/webui/apps/gazecontrol/gazecontrol.js
@@ -3,15 +3,37 @@ elation.require([], function() {
   elation.extend('janusweb.gazecontrol', class {
     constructor(object) {
       this.init()
+      this.teleporting = false
       elation.events.add(null, 'janusweb_script_frame', (e) => this.update(e) )
-      elation.events.add(null, 'player_setroom', this.init )
+      elation.events.add(null, 'player_setroom', (e) => this.init(e) )
       elation.events.add(null, 'webui_settings_store_init', this.extendSettingsUI )
+      elation.events.add(null, 'room_load_complete', (e) => setTimeout( () => this.teleporting = false, 5000 ))
+      elation.events.add(null, 'gazeenter', (ev) => { 
+        const obj = this.cursor_object
+        if( this.cursor && this.isClickable(obj) ){
+          this.cursor.setImage('hover')
+          this.cursor.fuseid = setTimeout( () => this.cursor.setImage('fuse'), 1000)
+          elation.events.fire({type: 'action-item-selected', data: this}); // XDG Sound
+        }
+      })
+      elation.events.add(null, 'gazeleave', (ev) => { 
+        if( this.cursor ) this.cursor.setImage('idle')
+      })
       elation.events.add(null, 'gazeactivate', (ev) => {
-        if( elation.config.get('webui.settings.vr.gaze_click') !== false ){
-          elation.events.fire({type: 'click', element: ev.data});
-          if (room.onClick) {
-            room.onClick(ev);
+        if( this.cooldown > 0 || this.teleporting) return
+
+        this.cooldown = 1000 
+        if( this.cursor ){ 
+          this.cursor.setImage('idle')
+          const obj = ev.data 
+          if( this.isClickable(obj) && elation.config.get('webui.settings.vr.gaze_click') !== false ){
+            if (room.onClick) { room.onClick(ev); }
+            elation.events.fire({type: 'click', element: obj._target, ev})
+            // this minimizes activating/gazing at another portal after teleporting
+            if( String(obj.componentname).match(/janusportal/) ) this.teleporting = true 
+            elation.events.fire({type: 'action-link-released', data: this}); // XDG Sound
           }
+          setTimeout( () => this.cooldown = 0, this.cooldown )
         }
       })
     }
@@ -34,36 +56,50 @@ elation.require([], function() {
     }
 
     init(e){
-      if( typeof room == undefined ){
-        if( !e ) throw 'cannot obtain room'
-        else room = e.data
-      }
-      this.scene     = elation.engine.instances.default.systems.world.scene['world-3d'] 
+      let newroom = e?.data || room
       if (!this.gazecaster) {
-        player.gazecaster = this.gazecaster = room.createObject('raycaster', {persist: false});
+        this.scene     = elation.engine.instances.default.systems.world.scene['world-3d'] 
+        player.gazecontrol = this
+        player.gazecaster = this.gazecaster = newroom.createObject('raycaster', {persist: false});
         player.gazecaster.enabled = elation.config.get('webui.settings.vr.gaze_control') == 'always'
-        player.head.add(player.gazecaster._target);
-        player.gazecaster = player.head.spawn('raycaster', null, {room, janus});
+        player.camera.add(player.gazecaster._target);
+        //player.gazecaster = player.head.spawn('raycaster', null, {room: newroom, janus});
         elation.events.add(this.gazecaster._target, 'raycastenter', elation.bind(this, this.handleGazeEnter));
         elation.events.add(this.gazecaster._target, 'raycastleave', elation.bind(this, this.handleGazeLeave));
         elation.events.add(this.gazecaster._target, 'raycastmove', elation.bind(this, this.handleGazeMove));
+        this.createReticle()
       } else {
-        player.gazecaster.setRoom(room);
+        player.gazecaster.setRoom(newroom);
       }
-
+      for( let i in this.cursors ){
+        // preload with disabled proxy so new rooms will not 
+        const cursor = this.cursors[i]
+        room.loadNewAsset("image", {id: cursor.src, src: cursor.src, proxy:false})
+      }
     }
 
     update(e){
+      const delta = e.data
+      let percent
+      const vrdisplay = player.engine.systems.render.views.main.vrdisplay ||
+                        player.engine.systems.render.views.xr 
+
+      if( janus.engine.client.xrplayer ){
+        const mode = elation.config.get('webui.settings.vr.gaze_control')
+        player.gazecaster.enabled =  mode == 'always' || 
+                                    ((!mode || mode == 'fallback') && !janus.engine.client.xrplayer.trackedobjects['hand_left']) ||
+                                    ((!mode || mode == 'fallback') && !janus.engine.client.xrplayer.trackedobjects['hand_right'])
+      }
       if (this.gaze && this.gaze.object) {
         var now = performance.now();
-        var gazetime = 1000;
+        var gazetime = room.gazetime 
         if (this.gaze.object.gazetime) {
           gazetime = this.gaze.object.gazetime;
         } else if (this.gaze.object.room && this.gaze.object.room.gazetime) {
           gazetime = this.gaze.object.room.gazetime;
         }
         var diff = now - this.gaze.start;
-        var percent = diff / gazetime;
+        percent = diff / gazetime;
         if (percent < 1) {
           this.gaze.object.dispatchEvent({type: 'gazeprogress', data: percent});
         } else if (!this.gaze.fired) {
@@ -72,11 +108,17 @@ elation.require([], function() {
           this.gaze.fired = true;
         }
       }
-      if( janus.engine.client.xrplayer ){
-        const mode = elation.config.get('webui.settings.vr.gaze_control')
-        player.gazecaster.enabled = (!mode || mode == 'always')
-                                    || (mode == 'fallback' && !janus.engine.client.xrplayer.trackedobjects['hand_left'])
-                                    || (mode == 'fallback' && !janus.engine.client.xrplayer.trackedobjects['hand_right'])
+      if( this.cursor ){
+        this.cursor.position.x = vrdisplay ? 0.015 : 0 // why?
+        this.cursor.visible = player.gazecaster.enabled
+        if( this.cursor.scale.x < this.cursor.current.scale ){
+          this.cursor.scale.x = this.cursor.scale.y = this.cursor.scale.z = this.cursor.scale.x + (0.8 * delta)
+        }
+
+        if( this.cursor.current.name == 'fuse' && elation.config.get('webui.settings.vr.gaze_click') !== false ){
+          this.cursor.opacity = 1 - percent
+          this.cursor.rotation.z += (150 * delta)
+        }
       }
     }
 
@@ -86,9 +128,15 @@ elation.require([], function() {
     }
     handleGazeEnter(ev){
       var obj = ev.data.object;
+      // dont enter gaze when user is clearly looking where to go
+      if( Math.abs(player.acceleration.x) > 0.1 ||
+          Math.abs(player.acceleration.y) > 0.1 ||
+          Math.abs(player.acceleration.y) > 0.1 
+      ){ return }
+
       if (obj && obj.dispatchEvent && ev.data.intersection) {
-        obj.dispatchEvent({type: 'gazeenter', data: ev.data.intersection});
         this.cursor_object = obj;
+        obj.dispatchEvent({type: 'gazeenter', data: ev.data.intersection});
 
         if (this.gaze) {
           this.cancelGaze();
@@ -117,6 +165,56 @@ elation.require([], function() {
         }
       }
     }
+
+    isClickable(obj){
+      return this.cursor && 
+             obj?.pickable && 
+             obj?.collidable && 
+             ( 
+               String(obj.componentname).match(/janus(portal|video|paragraph)/) ||
+               obj.video_id ||
+               obj.websurface_id ||
+               obj.eventlistenerproxies.click  
+             )
+
+    }
+
+    createReticle(){
+      const scale = 0.28
+      this.cursors = {
+        idle:  { name: 'idle', src: document.location.origin+'/media/assets/webui/apps/gazecontrol/images/reticle-idle.png', scale},
+        hover: { name: 'hover', src: document.location.origin+'/media/assets/webui/apps/gazecontrol/images/reticle-hover.png', scale},
+        fuse:  { name: 'fuse', src: document.location.origin+'/media/assets/webui/apps/gazecontrol/images/reticle-hover.png',  scale, scaleKeep:true},
+      }
+      this.cursor = player.createObject('object', {
+        pos: V(0,0,-3),
+        scale: V(scale,scale,scale),
+        col: '#FFF',
+        collidable: false,
+        pickable: false,
+        transparent: true,
+        lighting: false,
+        //depthTest: false,
+        //depthWrite: false,
+        proxy:false,
+        id: 'plane',
+        jsid: 'reticle',
+        renderorder: 10,
+      })
+      this.cursor.setImage = (cursorname) => {
+        this.cursor.current  = this.cursors[cursorname]
+        this.cursor.image_id = this.cursors[cursorname].src
+        if( !this.cursor.current.scaleKeep ){
+          this.cursor.scale.x  = this.cursor.scale.y = this.cursor.scale.z = scale * 0.5
+        }
+        this.cursor.opacity  = 1
+        this.cursor.rotation.z = 0
+        clearTimeout(this.cursor.fuseid)
+      }
+      this.cursor.setImage('idle')
+      player.camera.add(this.cursor)
+    }
+
   })
 });
 
