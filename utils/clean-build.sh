47548cd00d569bc1413814d2ef60f2b5f98e822e
Leon van Kammen <leonvankammen@gmail.com>
2026-07-26 16:08:56 +0200

    easy way for build to pick up on new assets/webui apps in /media

--- utils/clean-build.sh
+++ utils/clean-build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-
+# this script is basically you'd tell people to run to ensure a clean build.
+# since utils/build.sh will not copy new assets/webui apps in /media
 [ -e elation ] && rm -r elation
 [ -e node_modules ] && rm -r node_modules
 [ -d build ] && rm -r build
-read -p "[?] bleeding-edge dependencies? (y/n) " ok
-test "$ok" = y && rm package-lock.json
 npm install
 utils/build.sh
